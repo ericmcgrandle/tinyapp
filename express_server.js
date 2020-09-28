8442305aabb2c557e0da59b8f2f735fcd141d7ca
Eric McGrandle <ericmcgrandle@gmail.com>
2020-09-28 22:58:26 +0000

    added bodyParser

--- express_server.js
+++ express_server.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
+const bodyParser = require('body-parser');
 
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+//Database of shortened URL links
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//Listen to server on PORT
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+
+//app.get functions
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -26,9 +33,14 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
+app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
-
 });
+
+
+
