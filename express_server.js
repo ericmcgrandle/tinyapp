b7b12e42a459b0e7c3ecbb38107f45871eafe1b7
Eric McGrandle <ericmcgrandle@gmail.com>
2020-09-29 20:58:49 +0000

    Created branch, created login, displayed username, created logout

--- express_server.js
+++ express_server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser()); 
 app.set("view engine", "ejs");
 
 //Database of shortened URL links
@@ -34,7 +36,11 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
@@ -43,22 +49,12 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
-});
-
-app.post("/urls", (req, res) => {
-  const longURL = req.body.longURL;
-  let shortURL = generateRandomString();
-
-  if (Object.values(urlDatabase).indexOf(longURL) === -1) {
-    urlDatabase[shortURL] = longURL;
- } else {
-    shortURL = Object.keys(urlDatabase).find(key => urlDatabase[key] === longURL);
- }
-
- res.redirect(`/u/${shortURL}`);
-
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -73,6 +69,26 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.get(`/urls/:shortURL/update`, (req, res) => {
+  const shortURL = req.params.shortURL;
+  res.redirect(`/urls/${shortURL}`);
+});  
+
+
+//app.post functions
+app.post("/urls", (req, res) => {
+  const longURL = req.body.longURL;
+  let shortURL = generateRandomString();
+
+  if (Object.values(urlDatabase).indexOf(longURL) === -1) {
+    urlDatabase[shortURL] = longURL;
+ } else {
+    shortURL = Object.keys(urlDatabase).find(key => urlDatabase[key] === longURL);
+ }
+
+ res.redirect(`/u/${shortURL}`);
+
+});
 
 app.post(`/urls/:shortURL/delete`, (req, res) => {
   const shortURL = req.params.shortURL;
@@ -80,18 +96,24 @@ app.post(`/urls/:shortURL/delete`, (req, res) => {
   res.redirect('/urls');
 });
 
-app.get(`/urls/:shortURL/update`, (req, res) => {
-  const shortURL = req.params.shortURL;
-  res.redirect(`/urls/${shortURL}`);
-});  
-
 app.post(`/urls/:shortURL/update`, (req, res) => {
-
   const longURL = req.body.update;
   const shortURL = req.params.shortURL;
   urlDatabase[shortURL] = longURL;
-  res. redirect('/urls');
+  res.redirect('/urls');
 });
+
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
+
 
 
 
